80140f1d9891b69d36d611de54404d983475c1b7
elsantos121 <santys1629@gmail.com>
2025-06-10 23:49:49 -0300

    agrege comenarios

--- pythontest.py
+++ pythontest.py
@@ -1,3 +1,7 @@
+#en este archivo vamos a hacer un test de python
+#mostramos todas las operaciones matemáticas básicas
+
+
 print (5%5)
 print (5//5)
 print (5/5)
@@ -8,12 +12,16 @@ print (5**5)
 
 print ("hola,mundo!")
 
+#mostramos un ejemplo de input y output
 nombre = input ("¿como te llamas?")
 print(f"hola, {nombre}!")
 
+## Ejemplo de suma de dos números
+## Este código solicita al usuario dos números y muestra su suma.
 num1 = float(input("Ingresá el primer número: "))
 num2 = float(input("Ingresá el segundo número: "))
 
 suma = num1 + num2
 
+# Mostramos el resultado de la suma
 print(f"La suma de {num1} + {num2} es {suma}")
