01484655326dc9a694ce3a3a0355dd4637e22823
elsantos121 <santys1629@gmail.com>
2025-06-11 13:01:52 -0300

    tabla de multiplicar

--- maindos.py
+++ maindos.py
@@ -1,1 +1,10 @@
 #parte de santi
+#algoritmo que pide un numero y muestra la tabla de multiplicar de ese numero
+
+# Solicitar al usuario un número
+numero = int(input("Ingrese un número para mostrar su tabla de multiplicar: "))
+# Mostrar la tabla de multiplicar del número ingresado
+print(f"Tabla de multiplicar del {numero}:")
+for i in range(1, 11):
+    resultado = numero * i
+    print(f"{numero} x {i} = {resultado}")
